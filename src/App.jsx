0e0fc55b35ffa059fb9abe8cfbc22d05a4a35ae8
Shivani Tiwari <shivanitwr0803@gmail.com>
2026-01-25 16:40:00 +0530

    drag using rnd

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./app.scss";
 import Doc from "./components/Doc";
 import Nav from "./components/Nav";
+import MacWindow from "./components/windows/MacWindow";
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Doc />
+      <MacWindow /> 
     </main>
   )
 };
