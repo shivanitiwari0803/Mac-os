daa3af3419dca912d8bb235092dc7a4ec4c7e783
Shivani Tiwari <shivanitwr0803@gmail.com>
2026-01-26 23:31:06 +0530

    notes,resume done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Doc from "./components/Doc";
 import Nav from "./components/Nav";
 import MacWindow from "./components/windows/MacWindow";
 import Github from "./components/windows/Github";
+import Notes from "./components/windows/Notes";
+import Resume from "./components/windows/Resume";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Nav />
       <Doc />
       <Github />
+      <Notes />
+      <Resume />
     </main>
   )
 };
